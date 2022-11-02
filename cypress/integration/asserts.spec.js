0096dc9fb7341ee9061003a1f638c52b8bb71403
raisamoreno <moreno.raisa@gmail.com>
2022-11-02 15:59:54 -0300

    fim de assersões

--- cypress/integration/asserts.spec.js
+++ cypress/integration/asserts.spec.js
@@ -40,11 +40,46 @@ it( 'Object Equality', () => {
     expect({}).to.be.empty
 })
 
-
 it('Arrays', () => {
     const arr = [1,2,3]
     expect(arr).to.have.members([1,2,3])
     expect(arr).to.include.members([1,3])
     expect(arr).to.not.be.empty
     expect([]).to.be.empty
+})
+
+it('Types', () => {
+    const num = 1
+    const str = 'String'
+
+    expect(num).to.be.a('number')
+    expect(str).to.be.a('string')
+    expect({}).to.be.an('object')
+    expect([]).to.be.an('array')
+})
+
+it('String', () => {
+    const str = 'String de teste'
+
+    expect(str).to.be.equal('String de teste')
+    expect(str).to.have.length(15)
+    expect(str).to.match(/.{15}/)
+    expect(str).to.contain('de')
+    expect(str).to.match(/de/)
+    expect(str).to.match(/^String/)
+    expect(str).to.match(/teste$/)
+    expect(str).to.match(/\W+/)
+    expect(str).to.match(/\D+/)
+})
+
+it('Numbers', () => {
+    const number = 4
+    const floatNumber = 5.2123
+
+    expect(number).to.be.equal(4)
+    expect(number).to.be.above(3)
+    expect(number).to.be.below(7)
+    expect(floatNumber).to.be.equal(5.2123)
+    expect(floatNumber).to.be.closeTo(5.2, 0.1)
+    expect(floatNumber).to.be.above(5)
 })
